c00e83323bcb42b0ff3e84bb80b2b6c8c53a2174
Oyewusi Samuel Oluwatobi <41025327+suntwister@users.noreply.github.com>
2025-07-12 20:41:25 +0100

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Customers (
+	customer_id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(150) UNIQUE NOT NULL,
+	region VARCHAR(50) NOT NULL,
+	signup_date DATE NOT NULL
+);
